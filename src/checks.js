5c8a33fe8edb172798eba7712510eb37bd9c5ebe
sobadgirl <87839912+sobadgirl@users.noreply.github.com>
2022-02-16 15:50:12 +0800

    feat: tasks of automated testing support  only one check.

--- src/checks.js
+++ src/checks.js
@@ -17,6 +17,9 @@ class CheckResult {
 }
 
 
+const AUTOMATED_TESTING_HEADER  = "Automated Testing"
+
+
 let checkers = {
     lineChanges: pr => {
         // changes should less than 400 lines (include testing)
@@ -38,7 +41,7 @@ let checkers = {
     body: pr => {
         // pr body should contains these headers 
         const requireHeaders = ["Description", "Work Ticket", "Test Plan",
-         "Automated Testing", "Screenshots", "Checklist for PR owner", "Monitoring and Rollback Plan"]
+        AUTOMATED_TESTING_HEADER, "Screenshots", "Checklist for PR owner", "Monitoring and Rollback Plan"]
         if (!pr.body) return false;
         const tokens = marked.lexer(pr.body)
         const headers = tokens.filter(token => token.type === "heading" && token.depth <= 3).map(token => token.text)
@@ -52,9 +55,44 @@ let checkers = {
     },
     tasks: pr => {
         // all tasks in body should be completed
-        if (!pr.body) return false;
-        const tokens = marked.lexer(pr.body)
-        const taskItems = tokens.filter(token => token.type === "list").map(list => list.items).flat()
+        if (!pr.body) return new CheckResult({success: false, message: "Please leave something in body when start PR."});
+        let tokens = marked.lexer(pr.body)
+        tokens = tokens.map((token, index) => token.id = index)
+
+        let taskTokenOfAutomatedTesting = null
+        let automatedTestingHeadToken = null
+        tokens.forEach(token => {
+            // find tasks list between AUTOMATED_TESTING_HEADER and next header(section)
+            if (!automatedTestingHeadToken && token.type === "heading" && token.text === AUTOMATED_TESTING_HEADER) {
+                automatedTestingHeadToken = token
+            }
+            // when found tasks 
+            if (automatedTestingHeadToken && token.type === "list") {
+                taskTokenOfAutomatedTesting = token
+                return
+            }
+            // if for loop run to next header, means cloudn't found tasks between headers. 
+            if (automatedTestingHeadToken && token.type === "heading" && token.depth <= 3) {
+                return
+            }
+        })
+        if (!taskTokenOfAutomatedTesting) {
+            return new CheckResult({
+                success: false,
+                message: "Not found any check boxes under [Automated Testing] section, please follow the template."
+            })
+        }
+
+        // checking at least one box checked for Automated testing
+        const hasChecked = taskTokenOfAutomatedTesting.items.filter(item => item.task === true).map(item => item.checked).some(Boolean)
+        if (!hasChecked) {
+            return new CheckResult({
+                success: false,
+                message: "At least check one checkbox under [Automated Testing] section."
+            })
+        }
+
+        const taskItems = tokens.filter(token => token.type === "list" && token.id !== taskTokenOfAutomatedTesting.id).map(list => list.items).flat()
         const unCheckedItems = taskItems.filter(item => item.task === true).filter(item => item.checked === false)
         const success = unCheckedItems.length === 0
         return new CheckResult({
